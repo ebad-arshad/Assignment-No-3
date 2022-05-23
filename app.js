5f8f5bc7b72b213260e71e5809b40ed01d2470e1
M.Ebad Arshad <104248177+ebad-arshad@users.noreply.github.com>
2022-05-23 23:09:41 +0500

    completed assignment 2

---
+++ app.js
@@ -0,0 +1,69 @@
+// Chapter 6
+
+// Q1 
+
+// var userNum = +prompt("Enter any Number");
+// document.write("Result: <br /2>");
+// document.write("The value of a is: " + userNum + " <br /> <br /> <hr />");
+// document.write("<br />" + "The value of ++a is: " + ++userNum);
+// document.write("<br />" + "Now the value of a is: " + userNum + "<br />");
+// document.write("<br />" + "The value of a++ is: " + userNum++);
+// document.write("<br />" + "Now the value of a is: " + userNum + "<br />");
+// document.write("<br />" + "The value of --a is: " + --userNum);
+// document.write("<br />" + "Now the value of a is: " + userNum + "<br />");
+// document.write("<br />" + "The value of a-- is: " + userNum--);
+// document.write("<br />" + "Now the value of a is: " + userNum + "<br />");
+
+// Q2 
+
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write("--a; " + --a + "<br />");
+// document.write("--a - --b; " + (--a - --b) + "<br />");
+// document.write("--a - --b + ++b; " + (--a - --b + ++b) + "<br />");
+// document.write("--a - --b + ++b + b--; " + (--a - --b + ++b + b--) + "<br />");
+// document.write("Result");
+
+// Q3 
+
+// var userName = prompt("Enter your Name");
+// alert("Hello " + userName + "\nWelcome Back!");
+
+// Q4
+
+// var num = +prompt("Enter Number");
+// var i;
+// if (num === 0) {
+//     for (i = 1; i <= 10; i++) {
+//         document.write("5 x " + i + " = " + 5 * i + "<br />")
+//     }
+
+// }
+// else if (num === num) {
+//     for (i = 1; i <= 10; i++) {
+//         document.write(num + " x " + i + " = " + num * i + "<br />")
+//     }
+// }
+// else {
+//     alert("Type only Number")
+// }
+
+// Q5
+
+// var sub1 = prompt("Enter subject 1");
+// var sub2 = prompt("Enter subject 2");
+// var sub3 = prompt("Enter subject 3");
+// var tMarks = 100;
+// var mark1 = +prompt("Enter Marks of subject 1");
+// var mark2 = +prompt("Enter Marks of subject 2");
+// var mark3 = +prompt("Enter Marks of subject 3");
+// var result1 = mark1 * tMarks / 100; 
+// var result2 = mark2 * tMarks / 100; 
+// var result3 = mark3 * tMarks / 100; 
+// document.write(result1 + "%" + "<br />");
+// document.write(result2 + "%" + "<br />");
+// document.write(result3 + "%" + "<br />");
+
+
+
